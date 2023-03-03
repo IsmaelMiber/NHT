c67e474c5fdbe8a6c2dd972bd3a3cc15558caa21
Ismael Miber <ismael.miber@elcoach.me>
2023-03-03 03:29:17 +0200

    FIX

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
-import PersistentDrawerLeft from "./pages/Drawer"
+import PersistentDrawerLeft from "./pages/Drawer";
 function App() {
   return (
-    
-
     <Routes>
-    <Route
-              path="/"
-              element={<PersistentDrawerLeft />}
-            />
+      <Route path="/" element={<PersistentDrawerLeft />} />
     </Routes>
   );
 }
--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -2,10 +2,8 @@ import { combineReducers } from "redux";
 import EmloyeesReducer from "./EmployeesList/reducer";
 import { reducer as formReducer } from "redux-form";
 const rootReducer = combineReducers({
-    employees: EmloyeesReducer,
-    form: formReducer,
-    
-
+  employees: EmloyeesReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
